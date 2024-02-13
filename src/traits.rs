c052300fd95231522a45f5d83d54319b78f2309e
Omar Roman <omar.roman.2828@gmail.com>
2024-02-12 18:17:39 -0600

    traits

--- src/traits.rs
+++ src/traits.rs
@@ -7,7 +7,9 @@
  * según el ave, pero como no tenemos herencia
  * en rust, para ahorita usaremos solo traits,
  * getters y setters.
- */ 
+ */
+
+use std::ops::Add; 
 
 trait Ave {
     fn get_nombre(&self) -> String;
@@ -93,15 +95,142 @@ impl Nadador for Pinguino{
     }
 }
 
-// dyn indica que admite "cualquier implementacion de ave"
-fn mostrar_datos(ave: &dyn Ave){
+struct Avestruz{
+    nombre: String,
+    nombre_cientifico: String,
+    peso: f32,
+    tamanio_alas: f32,
+}
+
+impl Avestruz{
+    fn new(peso: f32, tamanio_alas: f32) -> Self{
+        Self{
+            nombre: String::from("Avestruz"),
+            nombre_cientifico: String::from("Struthio camelus"),
+            peso,
+            tamanio_alas
+        }
+    }
+}
+
+impl Ave for Avestruz{
+    fn get_nombre(&self) -> String {
+        self.nombre.clone()
+    }
+
+    fn get_nombre_cientifico(&self) -> String {
+        self.nombre_cientifico.clone()
+    }
+
+    fn get_peso(&self) -> f32 {
+        self.peso
+    }
+
+    fn get_tamanio_alas(&self) -> f32 {
+        self.tamanio_alas
+    }
+}
+
+impl Corredor for Avestruz{
+    fn correr_rapidamente(&self) {
+        println!("{} corre rápidamente", self.nombre);
+    }
+}
+
+// impl indica que admite "cualquier implementacion de ave"
+fn mostrar_datos(ave: &impl Ave){
     println!("Nombre: {}", ave.get_nombre());
     println!("Nombre cientifico: {}", ave.get_nombre_cientifico());
     println!("Peso: {}, Tamaño alas: {}", ave.get_peso(), ave.get_tamanio_alas());
+}
+
+// Impl es una avrebiacion de lo siguiente:
+// Pero esta notacion es más corta cuando tienes varios datos
+// de este tipo
+fn carrera<T: Corredor>(corredor1: &T, corredor2: &T) {
+    corredor1.correr_rapidamente();
+    corredor2.correr_rapidamente();
+}
+
+/* Notación where
+ * Podemos definidir los traits de mis tipos genéricos de forma más 
+ * ordenada utilizando la palabra reservada where
+ */ 
+fn _some_function<T, U, V>(_t: &T, _u: &U, _v: &V) -> i32
+    where
+    T: std::fmt::Display + Clone,
+    U: Clone,
+    V: Clone + Add + std::fmt::Display
+{
+    //TODO implementacion
+    1
+}
+
+// Podemos retornar una impl de ave
+fn _func1() -> impl Ave {
+    Pinguino::new(12.3, 4.0)
+}
+
+/* No es válido retornar dos impl diferentes de ave, o al menos
+ * no de esta forma:
+ *
+ * fn _func2(algo: bool) -> impl Ave {
+ *     if algo {
+ *         Pinguino::new(12.3, 4.0)
+ *     }
+ *     else {
+ *         Avestruz::new(12.3, 4.0)
+ *     }
+ * }
+ */ 
+
+// De esta forma sí, pero utiliza otros conceptos que se tocarán 
+// más adelante
+
+fn _func(switch: bool) -> Box<dyn Ave> {
+    if switch {
+        Box::new(Pinguino::new(12.3, 2.0))
+            
+    }
+    else {
+        Box::new(Avestruz::new(12.3, 2.0))
+    }
+}
+
+struct Dato<T>{
+    dato: T,
+}
+
+// Pdemos implementar diferentes cosas según los traits
+// de nuestro generico
+impl <T: Add<Output = T> + Copy> Dato<T> {
+    fn sumar(&mut self, dato: &T){
+        self.dato = self.dato + *dato;
+    }
+}
+
+impl <T: std::fmt::Display> Dato<T> {
+    fn print(&self){
+        println!("Dato: {}", self.dato);
+    }
 }
 
 pub fn _main(){
     let cody_maverick = Pinguino::new(12.3, 0.30);
 
     mostrar_datos(&cody_maverick);
+
+    let avesota = Avestruz::new(40.2, 0.20);
+
+    mostrar_datos(&avesota);
+
+    carrera(&avesota, &avesota);
+
+    let mut var: Dato<u32> = Dato{dato:2};
+    let dato: Dato<String> = Dato{dato: String::from("Hola")};
+
+    var.sumar(&12);
+    var.print();
+    dato.print();
+    // dato.sumar(&String::from("a")); //No válido porque string no es sumable
 }
